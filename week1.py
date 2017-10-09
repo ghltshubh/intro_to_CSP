1153da430f44eb12c00cce064eb897f6e5fe8287
ghltshubh <ghltshubh@gmail.com>
2017-10-09 00:37:26 -0500

    commit

--- week1.py
+++ week1.py
@@ -46,6 +46,50 @@ print("Longest substring in alphabhatical order is: ", maxSubstring)
 
 
 
+# return indices of contiguous substring of intList whose contituents sum clsest to n
+intList = [1,4,2,6,5,9]
+n = 13
+def closest_substring(intList, n):
+    closest = {}
+    for i in range(len(intList)):
+        sum = 0
+        for j in range(i, len(intList)):
+            sum += intList[j]
+            closest.update({(i,j):abs(sum - n)})
+    closest = sorted(closest.items(), key=lambda x:x[1])
+    return closest[0][0]
+
+# find integral point inside or on a triangle whose sum of distances from each 
+# vertex is minimum
+def area(x1,y1,x2,y2,x3,y3):
+    a = ((x1-x2)**2 + (y1-y2)**2)**0.5
+    b = ((x2-x3)**2 + (y2-y3)**2)**0.5
+    c = ((x3-x1)**2 + (y3-y1)**2)**0.5
+    S = (a+b+c)/2
+    A = (S*abs(S-a)*abs(S-b)*abs(S-c))**0.5
+    return A
+def minimum_sum(x1, y1, x2, y2, x3, y3):
+    distSum = {}
+    for x in range(min(round(x1),round(x2),round(x3)), 
+                   max(round(x1),round(x2),round(x3))+1):
+        for y in range(min(round(y1),round(y2),round(y3)), 
+                       max(round(y1),round(y2),round(y3))+1):
+            try:
+                if round(area(x,y,x2,y2,x3,y3) + area(x1,y1,x,y,x3,y3) + 
+                         area(x1,y1,x2,y2,x,y),4) == round(area(x1,y1,x2,y2,x3,y3),4):
+                    dist = ((x-x1)**2 + (y-y1)**2)**0.5 + ((x-x2)**2 + 
+                            (y-y2)**2)**0.5 + ((x-x3)**2 + (y-y3)**2)**0.5
+                    distSum.update({(x,y):dist})
+            except:
+                print("x: %f y:%f" %(x,y))   
+                print(area(x,y,x2,y2,x3,y3), area(x1,y1,x,y,x3,y3), area(x1,y1,x2,y2,x,y))
+    distSum = sorted(distSum.items(), key=lambda x:x[1])
+    if len(distSum) > 0:
+        return [i for i in distSum[0][0]]
+    else:
+        return [None, None]
+
+
 # square root
 x = 25
 epsilon = 0.01
